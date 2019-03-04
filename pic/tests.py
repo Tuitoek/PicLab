902dbe3712f264d8b65721e6417cbaeeb30450ba
Sarah Tuitoek <sjtoek@gmail.com>
2019-03-04 23:45:18 +0300

    Updated tests,urls.py and settings.py

--- pic/tests.py
+++ pic/tests.py
@@ -1,3 +1,92 @@
 from django.test import TestCase
+from .models import Image,Location,Category
 
 # Create your tests here.
+class LocationTestClass(TestCase):
+
+    #Set up method
+    def setUp(self):
+      self.mombasa=Location(image_location='Mombasa')
+
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.mombasa,Location))
+
+    # Testing save method
+    def test_save_location(self):
+        self.mombasa.save_location()
+        locations = Location.objects.all()
+        self.assertTrue(len(locations) > 0)
+
+    # Testing delete method
+    def test_delete_location(self):
+        self.mombasa.save_location()
+        self.mombasa.delete_location()
+        locations=Location.objects.all()
+        self.assertTrue(len(locations)==0)
+
+class ImageTestClass(TestCase):
+
+    """
+    Testclass for testing the image class
+    """
+
+    def setUp(self):
+        self.new_location=Location(name='Mombasa')
+        self.new_location.save_location()
+
+        self.new_category=Category(name='Food')
+        self.new_category.save_category()
+        self.new_image = Image(image_name='Fab',image_location=self.new_location,image_category=self.new_category)
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_image,Image))
+
+    # Testing Save Method
+    def test_save_method(self):
+        self.new_image.save_image()
+        editors = Editor.objects.all()
+        self.assertTrue(len(images) > 0)
+
+    def test_delete_method(self):
+        self.Image.delete_image()
+        self.assertEqual(len(),1)
+
+    def tearDown(self):
+        Category.objects.all().delete()
+        Location.objects.all().delete()
+        Image.objects.all().delete()
+
+    def test_get_imagesby_location(self):
+        images_by_location = Category.get_imagesby_location()
+        self.assertTrue(len(images_by_location) == 0)
+
+
+
+
+class CategoryTestClass(TestCase):
+    '''
+    Testclass for testing if the category class is working
+    '''
+    # Set up method
+    def setUp(self):
+        self.food = Category(image_category='Food')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.food,Category))
+
+    def test_save_category(self):
+        self.food.save_category()
+        categories=Category.objects.all()
+        self.assertTrue(len(categories)>0)
+
+    def test_delete_category(self):
+        self.food.save_category()
+        self.food.delete_category()
+        categories=Category.objects.all()
+        self.assertTrue(len(categories)==0)
+
+
+    def test_get_imagesby_category(self):
+        images_by_category = Category.get_imagesby_category()
+        self.assertTrue(len(images_by_category) == 0)
--- pic/views.py
+++ pic/views.py
@@ -1,8 +1,29 @@
 from django.shortcuts import render
 from django.http  import HttpResponse
 import datetime as dt
+from .models import Image
 
 # Create your views here.
 def landing(request):
     date = dt.date.today()
-    return render (request,'landing.html',{"date":date})
+    images = Image.objects.all()
+    return render (request,'landing.html',{"date":date,"images":images})
+
+def gallery(request):
+    images_item= Image.objects.all()
+    return render(request,'pictures.html',{"images":images_item})
+
+def search_results(request):
+
+    if 'images' in request.GET and request.GET["images"]:
+        search_term = request.GET.get("images")
+        searched_images= Image.search_by_title(search_term)
+        searched_images= Image.search_by_category(search_term)
+        searched_images= Image.search_by_location(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'search.html',{"message":message,"images": searched_images})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'search.html',{"message":message})
